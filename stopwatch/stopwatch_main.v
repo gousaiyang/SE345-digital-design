1c3af83e3b2f31fb0e2a5a0920045365fe7b2ef0
gousaiyang <gousaiyang@163.com>
2017-11-28 19:02:33 +0800

    stopwatch: partially fixed long pressing problem, still buggy

--- stopwatch/stopwatch_main.v
+++ stopwatch/stopwatch_main.v
@@ -1,15 +1,3 @@
-// Show current status (counting / stopped) on LED.
-module show_counting_status(time_counter, counting, led);
-	input [18:0] time_counter;
-	input        counting;
-	output [9:0] led;
-
-	// When stopped, all LEDs off.
-	// When counting, LED light spot moves to the right every second.
-	// When paused, LED light spot stays at the original position.
-	assign led = counting ? 1 << (9 - time_counter / 100 % 10) : 0;
-endmodule
-
 // Display time on sevensegs.
 module show_time(time_display, hex5, hex4, hex3, hex2, hex1, hex0);
 	input [18:0] time_display;
@@ -28,6 +16,18 @@ module show_time(time_display, hex5, hex4, hex3, hex2, hex1, hex0);
 	sevenseg_decimal(time_display % 10, hex0);
 endmodule
 
+// Show current status (counting / stopped) on LED.
+module show_counting_status(time_counter, counting, led);
+	input [18:0] time_counter;
+	input        counting;
+	output [9:0] led;
+
+	// When stopped, all LEDs off.
+	// When counting, LED light spot moves to the right every second.
+	// When paused, LED light spot stays at the original position.
+	assign led = counting ? 1 << (9 - time_counter / 100 % 10) : 0;
+endmodule
+
 // Main module.
 // Key usage:
 //     key3: Reset all states (will stop current counting).
@@ -35,6 +35,7 @@ endmodule
 //     key1: Pause display updating (but counting is still in process), display current time value and freeze.
 //     key0: Resume display updating.
 //     When stopped or paused, key1 and key0 will be disabled.
+// Current bug: time counter will stop when long pressing key1.
 module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1, hex0, led);
 	input         clk, key3, key2, key1, key0;
 	output [6:0]  hex5, hex4, hex3, hex2, hex1, hex0;
@@ -54,8 +55,28 @@ module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1,
 	show_time(time_display, hex5, hex4, hex3, hex2, hex1, hex0);
 	show_counting_status(time_counter, counting, led);
 
-	always @(negedge key3 or negedge key2) begin
+	// State change of time_counter and time_display.
+	always @(posedge clk or negedge key3 or negedge key1) begin
 		if (!key3) begin // Match sensitive signal list in if tests.
+			time_counter <= 0;
+			time_display <= 0;
+		end
+		else if (!key1) begin
+			if (counting && !paused)
+				time_display <= time_counter; // Update display.
+		end
+		else begin // clk posedge
+			if (counting && !paused) begin
+				time_counter = time_counter == max_time ? 0 : time_counter + 1; // Increment counter.
+				if (!freeze_display)
+					time_display = time_counter; // Update display.
+			end
+		end
+	end
+
+	// State change of counting and paused.
+	always @(negedge key3 or negedge key2) begin
+		if (!key3) begin
 			counting <= 0;
 			paused <= 0;
 		end
@@ -67,28 +88,18 @@ module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1,
 		end
 	end
 
-	always @(posedge clk or negedge key3 or negedge key1 or negedge key0) begin
+	// State change of freeze_display.
+	always @(negedge key3 or negedge key1 or negedge key0) begin
 		if (!key3) begin
-			time_counter <= 0;
-			time_display <= 0;
 			freeze_display <= 0;
 		end
 		else if (!key1) begin
-			if (counting && !paused) begin
+			if (counting && !paused)
 				freeze_display <= 1;
-				time_display <= time_counter;
-			end
 		end
-		else if (!key0) begin
+		else begin // key0 pressed
 			if (counting && !paused)
 				freeze_display <= 0;
-		end
-		else begin // clk posedge
-			if (counting && !paused) begin
-				time_counter = time_counter == max_time ? 0 : time_counter + 1;
-				if (!freeze_display)
-					time_display = time_counter;
-			end
 		end
 	end
 endmodule
