2fe1110adec240d35425cc26fd03871e729c5849
gousaiyang <gousaiyang@163.com>
2017-11-28 13:28:49 +0800

    stopwatch: reimplement debouncer (# is not synthesizable)

--- stopwatch/debouncer.v
+++ stopwatch/debouncer.v
@@ -1,17 +1,12 @@
 // Debounce a key on its posedge and negedge.
-module debouncer(keyin, keyout);
-	input      keyin;
+module debouncer(clk, keyin, keyout);
+	input      clk, keyin;
 	output reg keyout;
-	reg        keyf, keyn;
+	reg        keyp;
 
-	always @(keyin) begin
-		keyf = 0;
-		keyn = 1;
-		// Check until key state is consistent after 10ms.
-		while (keyf != keyn) begin
-			keyf = keyin;
-			#10000000 keyn = keyin;
-		end
-		keyout = keyf;
+	always @(posedge clk) begin
+		if (keyp == keyin)
+			keyout <= keyin;
+		keyp <= keyin;
 	end
 endmodule
