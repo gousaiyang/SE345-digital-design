534d37bba980f05dc29f3001595ad1ab5980a76a
gousaiyang <gousaiyang@163.com>
2017-11-30 14:08:19 +0800

    stopwatch: fix long pressing problem

--- stopwatch/stopwatch_main.v
+++ stopwatch/stopwatch_main.v
@@ -35,13 +35,12 @@ endmodule
 //     key1: Pause display updating (but counting is still in process), display current time value and freeze.
 //     key0: Resume display updating.
 //     When stopped or paused, key1 and key0 will be disabled.
-// Current bug: time counter will stop when long pressing key1.
 module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1, hex0, led);
 	input         clk, key3, key2, key1, key0;
 	output [6:0]  hex5, hex4, hex3, hex2, hex1, hex0;
 	output [9:0]  led;
 	reg    [18:0] time_counter, time_display;
-	reg           counting, paused, freeze_display;
+	reg           counting, paused, freeze_display, key1_last;
 	parameter max_time = 360000 - 1;
 
 	initial begin
@@ -50,27 +49,35 @@ module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1,
 		counting <= 0;
 		paused <= 0;
 		freeze_display <= 0;
+		key1_last <= 1;
 	end
 
 	show_time(time_display, hex5, hex4, hex3, hex2, hex1, hex0);
 	show_counting_status(time_counter, counting, led);
 
-	// State change of time_counter and time_display.
-	always @(posedge clk or negedge key3 or negedge key1) begin
+	// State change of time_counter.
+	always @(posedge clk or negedge key3) begin
 		if (!key3) begin // Match sensitive signal list in if tests.
 			time_counter <= 0;
+		end
+		else begin // clk posedge
+			if (counting && !paused)
+				time_counter = time_counter == max_time ? 0 : time_counter + 1; // Increment counter.
+		end
+	end
+
+	// State change of time_display.
+	always @(posedge clk or negedge key3 or negedge key1) begin
+		if (!key3) begin
 			time_display <= 0;
 		end
 		else if (!key1) begin
-			if (counting && !paused)
-				time_display <= time_counter; // Update display.
+			if (key1 != key1_last && counting && !paused) // Make sure state of key1 changes.
+				time_display = time_counter; // Update display.
 		end
 		else begin // clk posedge
-			if (counting && !paused) begin
-				time_counter = time_counter == max_time ? 0 : time_counter + 1; // Increment counter.
-				if (!freeze_display)
-					time_display = time_counter; // Update display.
-			end
+			if (counting && !paused && !freeze_display)
+				time_display = time_counter; // Update display.
 		end
 	end
 
@@ -93,7 +100,7 @@ module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1,
 		if (!key3) begin
 			freeze_display <= 0;
 		end
-		else if (!key1) begin
+		else if (!key1) begin // Note: Key priority: when key1 is long pressed, key0 press will not be responded.
 			if (counting && !paused)
 				freeze_display <= 1;
 		end
@@ -102,4 +109,8 @@ module stopwatch_main(clk, key3, key2, key1, key0, hex5, hex4, hex3, hex2, hex1,
 				freeze_display <= 0;
 		end
 	end
+
+	// State change of key1_last.
+	always @(posedge clk)
+		key1_last <= key1;
 endmodule
