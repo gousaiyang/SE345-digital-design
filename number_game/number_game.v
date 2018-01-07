0078e6b50ef3f6b482ca5830b84f0e119b6ede8e
gousaiyang <gousaiyang@163.com>
2018-01-07 22:52:17 +0800

    number_game: add scoreboard

--- number_game/number_game.v
+++ number_game/number_game.v
@@ -21,13 +21,14 @@ module number_game_main(clock, resetn, sw, key, hex5, hex4, hex3, hex2, hex1, he
 	wire   [6:0]  hex1_diff, hex0_diff;
 	wire   [6:0]  hex3_prepare, hex2_prepare;
 	wire   [6:0]  hex5_score, hex4_score, hex3_number, hex2_number, hex1_number, hex0_number;
+	wire   [6:0]  hex1_sb, hex0_sb;
 	wire   [9:0]  random;
 	wire   [9:0]  led_welcome, led_progress, led_success;
 	wire   [31:0] new_goal;
 	reg           resetn_downcount, downcount_started, key3_last;
 	reg    [2:0]  state;
 	reg    [3:0]  difficulty;
-	reg    [6:0]  goal, score;
+	reg    [6:0]  goal, score, highscore;
 	reg    [9:0]  number;
 
 	parameter secclk_N = 50000000;
@@ -39,6 +40,7 @@ module number_game_main(clock, resetn, sw, key, hex5, hex4, hex3, hex2, hex1, he
 	parameter code_D = 7'b0100001;
 	parameter code_F = 7'b0001110;
 	parameter code_G = 7'b1000010;
+	parameter code_H = 7'b0001001;
 	parameter code_I = 7'b1111001;
 	parameter code_L = 7'b1000111;
 	parameter code_O = 7'b1000000;
@@ -72,13 +74,15 @@ module number_game_main(clock, resetn, sw, key, hex5, hex4, hex3, hex2, hex1, he
 
 	display_success led_succ(flash_clock, resetn, led_success);
 
-	mux8x7 select_hex5(code_G, code_D, code_hf, hex5_score, code_hf, code_hf, code_off, code_off, state, hex5);
-	mux8x7 select_hex4(code_O, code_I, code_hf, hex4_score, code_S, code_F, code_off, code_off, state, hex4);
-	mux8x7 select_hex3(code_A, code_F, hex3_prepare, hex3_number, code_U, code_A, code_off, code_off, state, hex3);
-	mux8x7 select_hex2(code_L, code_F, hex2_prepare, hex2_number, code_C, code_I, code_off, code_off, state, hex2);
-	mux8x7 select_hex1(hex1_goal, hex1_diff, code_hf, hex1_number, code_C, code_L, code_off, code_off, state, hex1);
-	mux8x7 select_hex0(hex0_goal, hex0_diff, code_hf, hex0_number, code_hf, code_hf, code_off, code_off, state, hex0);
-	mux8x10 select_led(led_welcome, led_welcome, led_on, led_progress, led_success, led_off, led_off, led_off, state, led);
+	display_scoreboard disp_sb(highscore, hex1_sb, hex0_sb);
+
+	mux8x7 select_hex5(code_G, code_D, code_hf, hex5_score, code_hf, code_hf, code_H, code_off, state, hex5);
+	mux8x7 select_hex4(code_O, code_I, code_hf, hex4_score, code_S, code_F, code_I, code_off, state, hex4);
+	mux8x7 select_hex3(code_A, code_F, hex3_prepare, hex3_number, code_U, code_A, code_G, code_off, state, hex3);
+	mux8x7 select_hex2(code_L, code_F, hex2_prepare, hex2_number, code_C, code_I, code_H, code_off, state, hex2);
+	mux8x7 select_hex1(hex1_goal, hex1_diff, code_hf, hex1_number, code_C, code_L, hex1_sb, code_off, state, hex1);
+	mux8x7 select_hex0(hex0_goal, hex0_diff, code_hf, hex0_number, code_hf, code_hf, hex0_sb, code_off, state, hex0);
+	mux8x10 select_led(led_welcome, led_welcome, led_on, led_progress, led_success, led_off, led_welcome, led_off, state, led);
 
 	always @(posedge clock) begin
 		if (!resetn || !key1) begin
@@ -87,11 +91,15 @@ module number_game_main(clock, resetn, sw, key, hex5, hex4, hex3, hex2, hex1, he
 			downcount_started <= 0;
 			resetn_downcount <= 1;
 			key3_last <= 1;
+
+			if (!resetn)
+				highscore <= 0;
 		end else if (state == 0) begin
 			if (!key3) begin
 				goal <= new_goal;
 				state <= 1;
-			end
+			end else if (!key2)
+				state <= 6;
 		end else if (state == 1) begin
 			if (!key3 && key3_last) begin
 				difficulty <= new_difficulty;
@@ -118,6 +126,8 @@ module number_game_main(clock, resetn, sw, key, hex5, hex4, hex3, hex2, hex1, he
 				state <= 5;
 			end else if (sw == number) begin
 				score <= score + 1;
+				if (score + 1 > highscore)
+					highscore <= score + 1;
 				if (score + 1 >= goal)
 					state <= 4;
 				else begin
