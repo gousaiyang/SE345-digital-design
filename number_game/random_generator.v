8a952183a44f39ab10e12558a57c81f07db30756
gousaiyang <gousaiyang@163.com>
2018-01-08 23:28:07 +0800

    number_game: improve random number generator

--- number_game/random_generator.v
+++ number_game/random_generator.v
@@ -3,7 +3,7 @@ module rand10(clock, resetn, random);
 	output [9:0]  random;
 	wire   [31:0] counter;
 
-	assign random = counter[9:0];
+	assign random = counter[14:5]; // Strip last 5 bits to get more evenly distributed result.
 
 	cycles_counter cc(clock, resetn, counter);
 endmodule
